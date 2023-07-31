d1fb86b90ab1126d1ce797f1b3716ccc8e76a2f6
namnghia <113599209+namnghia@users.noreply.github.com>
2023-07-31 15:36:53 +0700

    Add files via upload

---
+++ frontend/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Facebook</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+  <header>
+    <h1>Welcome to Facebook</h1>
+  </header>
+  <nav>
+    <ul>
+      <li>Home</li>
+      <li>Profile</li>
+      <li>Friends</li>
+    </ul>
+  </nav>
+  <main>
+    <section>
+      <h2>News Feed</h2>
+      <div class="post">
+        <h3>John Doe</h3>
+        <p>This is a post.</p>
+      </div>
+      <div class="post">
+        <h3>Jane Smith</h3>
+        <p>This is another post.</p>
+      </div>
+    </section>
+    <aside>
+      <h2>Chat</h2>
+      <ul>
+        <li>Friend 1</li>
+        <li>Friend 2</li>
+        <li>Friend 3</li>
+      </ul>
+    </aside>
+  </main>
+  <footer>
+    <p>&copy; 2023 Facebook</p>
+  </footer>
+  <script src="script.js"></script>
+</body>
+</html>
